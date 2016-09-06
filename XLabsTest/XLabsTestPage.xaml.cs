72ecc61754a88cd706d0159f848e1705942d3ed7
Németh Gábor <gabor.nemeth.dev@gmail.com>
2016-09-06 10:17:14 +0200

    initial commit

---
+++ XLabsTest/XLabsTestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XLabsTest
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class XLabsTestPage : ContentPage
+	{
+		public XLabsTestPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
